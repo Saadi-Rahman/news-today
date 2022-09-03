6caab7eb74822842450734abd720f731bb48947d
Saadi-Rahman <sadi6280@gmail.com>
2022-09-04 05:20:58 +0600

    get categories from API and set them successfully

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log("connected");
+const loadAllCategories = () =>{
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(data => displayAllCategories(data.data.news_category))
+}
+
+
+const displayAllCategories = categories =>{
+    const newsCategories = document.getElementById("news-categories");
+    categories.forEach(category => {
+        console.log(category);
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <li class="nav-item px-md-3"><button class="btn btn-light fs-5 fw-semibold text-secondary">${category.category_name}</button></li>
+        `;
+        newsCategories.appendChild(li);
+    })
+}
+
+
+loadAllCategories();
